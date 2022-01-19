46ec3a706b29289a747abda8ac478d79e4be6ea7
Jabair <jabairk@gmail.com>
2022-01-19 15:46:30 -0500

    more homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,9 @@ import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { MatMenuModule } from '@angular/material/menu';
 import { HomeComponent } from './home/home.component';
 import { MatButtonModule } from '@angular/material/button';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faFilm } from '@fortawesome/free-solid-svg-icons';
 
 @NgModule({
   declarations: [
@@ -18,10 +21,15 @@ import { MatButtonModule } from '@angular/material/button';
     AppRoutingModule,
     NoopAnimationsModule,
     MatMenuModule,
-    MatButtonModule
+    MatButtonModule,
+    FontAwesomeModule
     
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+    constructor() {
+      library.add(faFilm)
+  }
+}
