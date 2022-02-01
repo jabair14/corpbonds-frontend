be1e33eecfc62fa05c538eaebaa9c18c0ad740e0
inbiii <omaidksquared@gmail.com>
2022-02-01 17:34:17 -0500

    light changes

--- src/app/usersFolder/accounts/accounts.component.ts
+++ src/app/usersFolder/accounts/accounts.component.ts
@@ -2,11 +2,25 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { CookieService } from 'ngx-cookie-service';
 import { UserService } from 'src/app/user.service';
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-accounts',
   templateUrl: './accounts.component.html',
   styleUrls: ['./accounts.component.scss'],
+  animations: [
+    trigger('bottomFade', [
+      state('void', style({ opacity: 0, transform: 'translateY(2rem)' })),
+      state('*', style({ opacity: 1, transform: 'translateY(0)' })),
+      transition(':enter', [animate(500), style({ opacity: 1 })]),
+    ]),
+  ],
 })
 export class AccountsComponent implements OnInit {
   redir: boolean = false;
