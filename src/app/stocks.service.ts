c2aeae48af20366e59e8a68d1e63972e8110531d
inbiii <omaidksquared@gmail.com>
2022-02-12 12:48:51 -0500

    add balance feature lite

--- src/app/stocks.service.ts
+++ src/app/stocks.service.ts
@@ -8,21 +8,21 @@ import { Integer } from 'aws-sdk/clients/eventbridge';
   providedIn: 'root',
 })
 export class StocksService {
-  //url: string = "https://stocks-microservice-app.herokuapp.com";
-  url: string = "http://localhost:3000";
+  url: string = 'https://stocks-microservice-app.herokuapp.com';
+  // url: string = "http://localhost:3000";
 
   constructor(private http: HttpClient) {}
 
   getStocks(): Observable<any> {
-    return this.http.get(`${this.url}/stocks`)
+    return this.http.get(`${this.url}/stocks`);
   }
 
   getStock(stockId: Integer): Observable<any> {
-    return this.http.get(`${this.url}/stocks/${stockId}`)
+    return this.http.get(`${this.url}/stocks/${stockId}`);
   }
-  
-  makeInvestment(investment:Investment): Observable<any> {
-    return this.http.post(`${this.url}/invest`, investment)
+
+  makeInvestment(investment: Investment): Observable<any> {
+    return this.http.post(`${this.url}/invest`, investment);
   }
 
   getInvestments(userId: String): Observable<any> {
